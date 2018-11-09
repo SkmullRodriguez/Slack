c3b1f9c39a3e6a6b3d33ad15ceef7a3fd1bbb1ce
Maria Madelyn Ruiz Claro <maria.ruiz@sv.cds>
2018-11-09 12:28:00 -0800

    Vistas en primer nivel x2

---
+++ 7-11-Slack/Controllers/EMDController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _7_11_Slack.Controllers
+{
+    public class EMDController : Controller
+    {
+        // GET: EMD
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Biologicos()
+        {
+            return View();
+        }
+        public ActionResult Hidrologicos()
+        {
+            return View();
+        }
+        public ActionResult Metereologicos()
+        {
+            return View();
+        }
+        public ActionResult Geologicos()
+        {
+            return View();
+        }
+        public ActionResult Inudaciones()
+        {
+            return View();
+        }
+    }
+}
