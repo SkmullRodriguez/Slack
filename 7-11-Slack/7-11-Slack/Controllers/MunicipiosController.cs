bff554ea958188575a478a97a25c8297ed8faa4e
William Alberto Rodriguez Diaz <william.rodriguez@sv.cds>
2018-11-09 11:41:58 -0600

    test anidado

---
+++ 7-11-Slack/7-11-Slack/Controllers/MunicipiosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _7_11_Slack.Models;
+
+namespace _7_11_Slack.Controllers
+{
+    public class MunicipiosController : Controller
+    {
+        private sl_baseEntities db = new sl_baseEntities();
+
+        // GET: Municipios
+        public ActionResult Index()
+        {
+            var municipios = db.Municipios.Include(m => m.Departamento1);
+            return View(municipios.ToList());
+        }
+
+        // GET: Municipios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Municipio municipio = db.Municipios.Find(id);
+            if (municipio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(municipio);
+        }
+
+        // GET: Municipios/Create
+        public ActionResult Create()
+        {
+            ViewBag.Departamento = new SelectList(db.Departamentoes, "IdDepartamento", "Departamento1");
+            return View();
+        }
+
+        // POST: Municipios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdMunicipio,Departamento,Municipio1")] Municipio municipio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Municipios.Add(municipio);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Departamento = new SelectList(db.Departamentoes, "IdDepartamento", "Departamento1", municipio.Departamento);
+            return View(municipio);
+        }
+
+        // GET: Municipios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Municipio municipio = db.Municipios.Find(id);
+            if (municipio == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Departamento = new SelectList(db.Departamentoes, "IdDepartamento", "Departamento1", municipio.Departamento);
+            return View(municipio);
+        }
+
+        // POST: Municipios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdMunicipio,Departamento,Municipio1")] Municipio municipio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(municipio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Departamento = new SelectList(db.Departamentoes, "IdDepartamento", "Departamento1", municipio.Departamento);
+            return View(municipio);
+        }
+
+        // GET: Municipios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Municipio municipio = db.Municipios.Find(id);
+            if (municipio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(municipio);
+        }
+
+        // POST: Municipios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Municipio municipio = db.Municipios.Find(id);
+            db.Municipios.Remove(municipio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 7-11-Slack/7-11-Slack/Controllers/SubCategoriasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _7_11_Slack.Models;
+
+namespace _7_11_Slack.Controllers
+{
+    public class SubCategoriasController : Controller
+    {
+        private sl_baseEntities db = new sl_baseEntities();
+
+        // GET: SubCategorias
+        public ActionResult Index()
+        {
+            var subCategorias = db.SubCategorias.Include(s => s.Categoria);
+            return View(subCategorias.ToList());
+        }
+
+        // GET: SubCategorias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategoria subCategoria = db.SubCategorias.Find(id);
+            if (subCategoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subCategoria);
+        }
+
+        // GET: SubCategorias/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCategoria = new SelectList(db.Categorias, "IdCategoria", "NombreCategoria");
+            return View();
+        }
+
+        // POST: SubCategorias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdSubCategoria,IdCategoria,NombreSubCategoria,Estado")] SubCategoria subCategoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SubCategorias.Add(subCategoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdCategoria = new SelectList(db.Categorias, "IdCategoria", "NombreCategoria", subCategoria.IdCategoria);
+            return View(subCategoria);
+        }
+
+        // GET: SubCategorias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategoria subCategoria = db.SubCategorias.Find(id);
+            if (subCategoria == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdCategoria = new SelectList(db.Categorias, "IdCategoria", "NombreCategoria", subCategoria.IdCategoria);
+            return View(subCategoria);
+        }
+
+        // POST: SubCategorias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdSubCategoria,IdCategoria,NombreSubCategoria,Estado")] SubCategoria subCategoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subCategoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCategoria = new SelectList(db.Categorias, "IdCategoria", "NombreCategoria", subCategoria.IdCategoria);
+            return View(subCategoria);
+        }
+
+        // GET: SubCategorias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategoria subCategoria = db.SubCategorias.Find(id);
+            if (subCategoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subCategoria);
+        }
+
+        // POST: SubCategorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SubCategoria subCategoria = db.SubCategorias.Find(id);
+            db.SubCategorias.Remove(subCategoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
